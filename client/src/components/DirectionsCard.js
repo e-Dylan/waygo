dbaba3804adca1b450006ea59e7c65dd60cb359d
pDylanSmith <dylan@alignsoft.ca>
2020-10-19 23:23:30 -0400

    built directions api and integration with directions ui.

---
+++ client/src/components/DirectionsCard.js
@@ -0,0 +1,156 @@
+import React from "react";
+
+import { Card } from 'reactstrap';
+
+import directionsBuildingIcon from "../resources/maki-icons/building-alt1-15.svg";
+
+import '../App.css'
+import './components-styles/MapComponent.css';
+
+class DirectionsCard extends React.Component {
+
+	constructor(props) {
+		super(props);
+	}
+
+	state = {
+
+	}
+
+	render() {
+		return(
+			<Card body className="map-hud-card directions-card">
+				<div className="dirs-title">
+					<div className="dirs-burger"
+						onClick={this.props.mapComponent.moveHomeDock}>
+						<div className="search-bar-button__burger"></div>
+					</div>
+
+					<div 
+						className="show-loc-searchbar-button"
+						onClick={() => {
+							this.props.mapComponent.hideDirections();
+							this.props.mapComponent.showLocation();
+						}}
+					>
+						<img src={directionsBuildingIcon}></img>
+					</div>
+
+				</div>
+				<div className="dirs-fromto">
+					<div className="from-bar">
+						<a className="dirs-label"></a>
+						<input 
+							className="dirs-input-bar"
+							id="from-dirs-bar"
+							placeholder="from"
+							onFocus={(e) => {
+								this.props.mapComponent.showDirsSearchResults({dir: "from"});
+							}}
+							onChange={(e) => {
+								if (e.target.value.length > 2) {
+									// make request for locations only if 3+ chars
+									this.props.mapComponent.forwardGeocode({
+										endpoint: "mapbox.places", 
+										query: e.target.value, 
+										autocomplete: true, 
+										displayActiveMarker: false
+									});
+
+									if (e.target.value.length < 1) {
+										// If clearing search bar, hide and empty search results.
+										this.props.mapComponent.hideDirsSearchResults({dir: "from", reset: true });
+									} else {
+										this.props.mapComponent.showDirsSearchResults({dir: "from"});
+									}
+								}
+							}}
+						>
+
+						</input>
+						{/* <img src={directionsCarIcon}></img> */}
+					</div>
+					<div className="to-bar">
+						<a className="dirs-label"></a>
+						<input 
+							className="dirs-input-bar"
+							id = "to-dirs-bar"
+							placeholder="to"
+							onFocus={(e) => {
+								this.props.mapComponent.showDirsSearchResults({dir: "to"});
+							}}
+							onChange={(e) => {
+								if (e.target.value.length > 2) {
+									// make request for locations only if 3+ chars
+									this.props.mapComponent.forwardGeocode({
+										endpoint: "mapbox.places", 
+										query: e.target.value, 
+										autocomplete: true, 
+										displayActiveMarker: false
+									});
+
+									if (e.target.value.length < 1) {
+										// If clearing search bar, hide and empty search results.
+										this.props.mapComponent.hideDirsSearchResults({dir: "to", reset: true });
+									} else {
+										this.props.mapComponent.showDirsSearchResults({dir: "to"});
+									}
+								}
+							}}
+						>
+						{/* <img src={directionsCarIcon}></img> */}
+							
+						</input>
+					</div>
+				</div>
+
+				{ this.props.mapComponent.state.showDirsFromSearchResults && this.props.mapComponent.state.searchResultItems.length > 0 &&
+								
+					<Card className="search-results-bg-card dirs-from-search-results">
+						{ this.props.mapComponent.state.searchResultItems.map(item =>
+
+							<div 
+							className="search-result-div"
+							onClick={ () => {
+								this.props.mapComponent.hideDirsSearchResults({dir: "to", reset: false });
+								// set camera at location on origin marker placement, not item click
+								
+							}}
+							key={Math.random()}
+							title={item.place_name}
+							>
+							{item.place_name}
+							</div>
+
+						) }
+					</Card>
+				}
+
+				{ this.props.mapComponent.state.showDirsToSearchResults && this.props.mapComponent.state.searchResultItems.length > 0 &&
+
+					<Card className="search-results-bg-card dirs-to-search-results">
+						{ this.props.mapComponent.state.searchResultItems.map(item =>
+
+							<div 
+							className="search-result-div"
+							onClick={ () => {
+								this.props.mapComponent.hideDirsSearchResults({ reset: false });
+								// set camera at location on origin marker placement, not item click
+								
+							}}
+							key={Math.random()}
+							title={item.place_name}
+							>
+							
+							</div>
+
+						) }
+					</Card>
+					
+				}
+			</Card>
+		);
+	}
+}
+
+export default DirectionsCard;
---
+++ client/src/components/MapSearchBar.js
@@ -0,0 +1,142 @@
+import React from "react";
+
+import { Card } from 'reactstrap';
+
+import directionsCarIcon from "../resources/maki-icons/car-15.svg";
+import citySearchIcon from "../resources/maki-icons/building-alt1-15.svg";
+
+import '../App.css'
+import './components-styles/MapComponent.css';
+
+class MapSearchBar extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+
+	state = {
+
+	};
+
+	render() {
+		return(
+			<div>
+				<Card body className="map-search-bar-card">
+					<div className="search-bar-button"
+						onClick={this.props.mapComponent.moveHomeDock}>
+						<div className="search-bar-button__burger"></div>
+					</div>
+					
+					<input 
+						className="map-search-bar-search input-search-bar"
+						placeholder="where to?"	
+						onFocus={(e) => {
+							this.props.mapComponent.showLocationSearchResults();
+						}}
+						onChange={(e) => {
+							// make api call to get best 5 results using search text,
+							// set search result text to response data.
+							
+							if (e.target.value.length > 2) {
+								// only send api request if query is >= 3 chars.
+								this.props.mapComponent.forwardGeocode({
+									endpoint: "mapbox.places", 
+									query: e.target.value, 
+									autocomplete: true, 
+									displayActiveMarker: false
+								});
+							}
+
+							if (e.target.value.length < 1) {
+								// If clearing search bar, hide and empty search results.
+								this.props.mapComponent.hideLocationSearchResults({reset: true});
+							} else {
+								this.props.mapComponent.showLocationSearchResults();
+							}
+							
+						}}
+						>
+					</input>
+					
+					<div 
+						className="show-dirs-searchbar-button"
+						onClick={() => {
+							this.props.mapComponent.hideLocation();
+							this.props.mapComponent.showDirections();
+						}}
+					>
+						<img src={directionsCarIcon}></img>
+					</div>
+				</Card>
+
+				{ this.props.mapComponent.state.showLocationSearchResults && this.props.mapComponent.state.showLocation &&
+
+					<Card body className="search-results-bg-card">
+
+						{ this.props.mapComponent.state.searchResultItems.map(searchResult =>
+							// scrape api data and fill the state search results array
+							// display state search results in jsx
+								<div 
+								className="search-result-div"
+								onClick={ () => {						
+									// stop showing search results when location is clicked.
+									this.props.mapComponent.hideLocationSearchResults({reset: false});
+									// call flyTo method to move to this position.
+									if (searchResult != null) {
+										const lng = searchResult.lng;
+										const lat = searchResult.lat;
+										this.props.mapComponent._flyTo({ lng: lng, lat: lat, zoom: 12, displayActiveMarker: true });
+										// scrape location data
+										this.props.mapComponent.compileActiveLocationData(searchResult);
+										this.props.mapComponent.showLocationCard(this.props.mapComponent.state.activeLocationData);
+									}
+									
+									// MAKE FUNCTION: DISPLAY LOCATION CARD
+									// used for reverse geocoding AND flyTo search clicks
+									// displays location in a card with image and data using lngLat
+									// can call in both clicking on map, and from here with its lngLat
+									// also displays location marker, instead of _flyTo().
+								}} 
+								key={Math.random()}
+								title={searchResult.place}
+								>
+									<div>
+										<img src={ citySearchIcon } className="search-result-icon"></img>
+									</div>
+									{ searchResult.place &&
+										<span className="search-result-place" title={searchResult.place}>
+											{searchResult.place}
+										</span>
+									}
+									{ searchResult.address &&
+										<span className="search-result-address">
+											{searchResult.address}
+										</span>
+									}
+									{ searchResult.city &&
+										<span className="item-city">
+											{searchResult.city}
+										</span>
+									}
+									{ searchResult.country &&
+										<span className="region-country"> 
+												{searchResult.region}, {searchResult.country}
+										</span>
+									}
+
+								</div>
+							)
+						}
+					</Card>
+				}
+			</div>
+			
+
+			
+		)
+	}
+
+
+
+}
+
+export default MapSearchBar;
