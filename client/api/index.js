fae38bdb2dd4033ccaa98da2266b6bc05e55049a
e-Dylan <dylan@alignsoft.ca>
2020-12-13 20:52:11 -0500

    migrating serverless

--- client/api/index.js
+++ client/api/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
-//const morgan = require('morgan'); -> Switched to volleyball
+// const morgan = require('morgan'); -> Switched to volleyball
 // const helmet = require('helmet');
-const cors = require('cors'); //
+// const cors = require('cors');
 // const volleyball = require('volleyball');
 // const bcrypt = require('bcryptjs');
 // const Joi = require('joi');
@@ -18,19 +18,6 @@ const sql_db = require('./sql_db');
 require('dotenv').config();
 
 // const middlewares = require('./middlewares');
-
-// Require all api backend serivces (waymessages, users, index.js)
-// to be used with express app.
-
-const router = express.Router();
-
-// Import routers
-const users_router = require('./users');
-const saved_locations_router = require('./savedLocations');
-
-// Combine routers
-router.use('/', users_router);
-router.use('/', saved_locations_router);
 
 const app = express();
 
@@ -68,7 +55,117 @@ app.use(session({
 // 	res.send("Served '/' on main app /api. ");
 // });
 
-app.use('/api', router);
+app.post('/api/isLoggedIn', (req, res) => {
+	// session.userID is set to db id to the req when user logs in.
+	console.log("checking if logged in - session: " + req.session);
+	if (req.session.userID) {
+		let cols = [req.session.userID];
+		sql_db.query('SELECT * FROM user WHERE id = ? LIMIT 1', cols, (err, data, fields) => {
+			// if the session user's id matches one in the db, they're logged in.
+			if (data && data.length === 1) {
+				res.json({
+				success: true,
+				username: data[0].username
+				});
+				return true;
+			}
+			else // no data matches from the db, no user.
+			{
+				res.json({
+					success: false
+				});
+			}
+
+		});
+	}
+	else // User has no session id.
+	{
+		res.json({
+			success: false
+		});
+	}
+
+});
+
+app.post('/api/login', (req, res) => {
+
+	console.log("session id: " + req.session.id);
+
+	let username = req.body.username.toLowerCase();
+	let password = req.body.password;
+
+	// User login info validation
+	if (username.length > 16 || password.length > 16) {
+		res.json({
+		success: false,
+		msg: 'Username and password must be shorter than 16 characters.'
+		});
+		return;
+	}
+
+	// Check if username exists in db
+	let cols = [username];
+	sql_db.query("SELECT * FROM user WHERE username = ? LIMIT 1", cols, (err, data, fields) => {
+		if (err) {
+		// No username in the database matching login username
+		res.json({
+			success: false,
+			msg: 'User does not exist.'
+		});
+		return;
+		}
+
+		// Found a user with this username
+		// data = data array returned from db query
+		if (data && data.length === 1) {
+		// compare username's hashed password with db password
+		bcrypt.compare(password, data[0].password, (bcryptErr, verified) => {
+			// password is the same
+			if (verified) {
+
+			// Log the user in. Start a session for user.
+			req.session.userID = data[0].id;
+			req.session.save((err) => {
+				if (err) {
+				console.log(err);
+				}
+			});
+			const userData = {
+				username: data[0].username,
+				password: password,
+				email: data[0].email,
+				session: req.session,
+			}
+			console.log(`User successfully logging in: ${JSON.stringify(userData, null, 4)}`);
+
+			res.json({
+				success: true,
+				username: data[0].username,
+			});
+
+			return;
+			}
+
+			else {
+			// Not verified, wrong password.
+			res.json({
+				success: false,
+				msg: 'Incorrect password.'
+			});
+			}
+
+		});
+		}
+		else // User does not exist in database.
+		{
+		res.json({
+			success: false,
+			msg: 'User not found. Please try again.'
+		});
+		}
+		
+	});
+})
 
 // app.use(middlewares.notFound);
 // app.use(middlewares.errorHandler);
--- client/api/users.js
+++ client/api/users.js
@@ -79,46 +79,46 @@ app.post('/register', (req, res) => {
 
 app.post('/login', (req, res) => {
 
-    console.log("session id: " + req.session.id);
-  
-    let username = req.body.username.toLowerCase();
-    let password = req.body.password;
-  
-    // User login info validation
-    if (username.length > 16 || password.length > 16) {
-      res.json({
-        success: false,
-        msg: 'Username and password must be shorter than 16 characters.'
-      });
-      return;
-    }
-  
-    // Check if username exists in db
-    let cols = [username];
-    sql_db.query("SELECT * FROM user WHERE username = ? LIMIT 1", cols, (err, data, fields) => {
-      if (err) {
-        // No username in the database matching login username
-        res.json({
-          success: false,
-          msg: 'User does not exist.'
-        });
-        return;
-      }
-  
-      // Found a user with this username
-      // data = data array returned from db query
-      if (data && data.length === 1) {
-        // compare username's hashed password with db password
-        bcrypt.compare(password, data[0].password, (bcryptErr, verified) => {
-          // password is the same
-          if (verified) {
-  
-            // Log the user in. Start a session for user.
-            req.session.userID = data[0].id;
-            req.session.save((err) => {
-              if (err) {
-                console.log(err);
-              }
+	console.log("session id: " + req.session.id);
+
+	let username = req.body.username.toLowerCase();
+	let password = req.body.password;
+
+	// User login info validation
+	if (username.length > 16 || password.length > 16) {
+		res.json({
+		success: false,
+		msg: 'Username and password must be shorter than 16 characters.'
+		});
+		return;
+	}
+
+	// Check if username exists in db
+	let cols = [username];
+	sql_db.query("SELECT * FROM user WHERE username = ? LIMIT 1", cols, (err, data, fields) => {
+		if (err) {
+		// No username in the database matching login username
+		res.json({
+			success: false,
+			msg: 'User does not exist.'
+		});
+		return;
+		}
+
+		// Found a user with this username
+		// data = data array returned from db query
+		if (data && data.length === 1) {
+		// compare username's hashed password with db password
+		bcrypt.compare(password, data[0].password, (bcryptErr, verified) => {
+			// password is the same
+			if (verified) {
+
+			// Log the user in. Start a session for user.
+			req.session.userID = data[0].id;
+			req.session.save((err) => {
+				if (err) {
+				console.log(err);
+				}
 			});
 			const userData = {
 				username: data[0].username,
@@ -126,36 +126,36 @@ app.post('/login', (req, res) => {
 				email: data[0].email,
 				session: req.session,
 			}
-            console.log(`User successfully logging in: ${JSON.stringify(userData, null, 4)}`);
-  
-            res.json({
-              success: true,
-              username: data[0].username,
-            });
-  
-            return;
-          }
-  
-          else {
-            // Not verified, wrong password.
-            res.json({
-              success: false,
-              msg: 'Incorrect password.'
-            });
-          }
-  
-        });
-      }
-      else // User does not exist in database.
-      {
-        res.json({
-          success: false,
-          msg: 'User not found. Please try again.'
-        });
-      }
-      
-    });
-  })
+			console.log(`User successfully logging in: ${JSON.stringify(userData, null, 4)}`);
+
+			res.json({
+				success: true,
+				username: data[0].username,
+			});
+
+			return;
+			}
+
+			else {
+			// Not verified, wrong password.
+			res.json({
+				success: false,
+				msg: 'Incorrect password.'
+			});
+			}
+
+		});
+		}
+		else // User does not exist in database.
+		{
+		res.json({
+			success: false,
+			msg: 'User not found. Please try again.'
+		});
+		}
+		
+	});
+})
   
 app.post('/logout', (req, res) => {
 	var cols = [req.session.userID];
@@ -190,7 +190,7 @@ app.post('/logout', (req, res) => {
 	});
 });
   
-router.post('/api/isLoggedIn', (req, res) => {
+router.post('/isLoggedIn', (req, res) => {
 	// session.userID is set to db id to the req when user logs in.
 	console.log("checking if logged in - session: " + req.session);
 	if (req.session.userID) {
@@ -221,5 +221,3 @@ router.post('/api/isLoggedIn', (req, res) => {
 	}
 
 });
-
-module.exports = router;
